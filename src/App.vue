33055d49008dc419a793126a241b071968568963
Svetozar Iliev <svetozariliev13@gmail.com>
2019-05-08 16:16:11 +0300

    Little space added

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
 <div id="app">
   <div id="nav">
-    <router-link to="/">Home</router-link>|
-    <router-link to="/store">Catalog</router-link>|
-    <router-link to="/sortedByLikes">Sorted by Likes |</router-link>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/store">Catalog</router-link> |
+    <router-link to="/sortedByLikes">Sorted by Likes</router-link> |
     <router-link to="/contactUs">Contact us</router-link>
   </div>
   <router-view />
